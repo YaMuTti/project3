c39f6e2f53b49a64a2030caa69cd7a75788afaec
HAKYUNG-PC\Ha Kyung JO <somehow55@gmail.com>
2021-07-26 23:35:00 +0900

    init commit

---
+++ src/main/java/com/koreait/workinapp/controller/admin/AdminController.java
@@ -0,0 +1,37 @@
+package com.koreait.workinapp.controller.admin;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class AdminController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(AdminController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+			
+		return "home";
+	}
+	
+	//관리자 메인 페이지 요청 처리
+	@RequestMapping(value = "/admin", method = RequestMethod.GET)
+	public String adminMain() {
+		return "admin/admin_main";
+	}
+}
